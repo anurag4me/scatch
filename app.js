6f7edee768a55ff540b4fb9713fba63a54f06748
anurag4me <anuragmishra5033@gmail.com>
2025-03-11 19:49:21 +0530

    Routes Created

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+const userRouter = require("./routes/user.routes");
+const productRouter = require("./routes/product.routes");
+const ownerRouter = require("./routes/owner.routes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+connectDB(process.env.DB_CONNECT)
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log("Mongo error", err));
+
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is working fine" });
+  return res.send("<h1>Hello World!</h1>");
 });
+
+// Routes
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/owner", ownerRouter);
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
